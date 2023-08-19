3909fbfe00d059904aaf293aacd8914ef5b0f441
joaoadryan <joaovieira464@gmail.com>
2023-08-19 17:55:40 -0300

    conceito de FINAL na variavel

--- src/tiposVariáveis.java
+++ src/tiposVariáveis.java
@@ -1,5 +1,18 @@
 public class tiposVariáveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       /*  double saralarioMinimo = 2500;
+        short numeroCUrto = 1;
+        int numeroNormal = numeroCUrto;
+        short numeroCurto2 =(short) numeroNormal;
+        
+*/
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
     }
 }

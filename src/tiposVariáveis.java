6baa7c2041e8ec9feeb122d40850025baac03a3b
joaoadryan <joaovieira464@gmail.com>
2023-08-19 18:21:08 -0300

    novo arquivo

--- src/tiposVariáveis.java
+++ src/tiposVariáveis.java
@@ -1,10 +1,12 @@
+import java.sql.Date;
+
 public class tiposVariáveis {
     public static void main(String[] args) throws Exception {
        /*  double saralarioMinimo = 2500;
         short numeroCUrto = 1;
         int numeroNormal = numeroCUrto;
         short numeroCurto2 =(short) numeroNormal;
-        
+         
 */
         int numero = 5;
         numero = 10;
@@ -13,6 +15,7 @@ public class tiposVariáveis {
         final double VALOR_DE_PI = 3.14;
 
         System.out.println(VALOR_DE_PI);
+        
 
     }
 }
